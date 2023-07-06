ab0de029f11e9efddf0774fa1e1af40d02047ef8
Stanislau Zakharevich <stanislau.zakharevich@gmail.com>
2023-07-06 20:38:47 +0200

    fix(Navigation): redirect to another domain refactor

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -3,21 +3,29 @@ import {Routes, Route, useLocation} from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import HolyMassPage from "../Pages/HolyMassPage";
 
+const catholicDomain = 'www.catholic-dev.store';
+const churchDomain = 'www.holymass-dev.store';
+
 const Navigation = () => {
   const location = useLocation();
+  const [isFirstTime, setFirstTime] = React.useState(true);
 
   useEffect(() => {
-    if (window.location.hostname === "www.holymass-dev.store" && window.location.pathname !== '/') {
-      window.location.assign(`http://www.catholic-dev.store${window.location.pathname}`);
-    } else if (window.location.pathname === '/holy-mass') {
-      window.location.assign('http://www.holymass-dev.store');
+    if (!isFirstTime) {
+      if (window.location.hostname === churchDomain) {
+        window.location.assign(`http://${catholicDomain}${window.location.pathname}`);
+      }
+      if (window.location.hostname === catholicDomain && window.location.pathname === '/holy-mass') {
+        window.location.assign(`http://${churchDomain}`);
+      }
     }
+    setFirstTime(false);
   }, [location]);
 
   return (
     <div className="body-container">
       <Routes>
-        {window.location.hostname === "www.holymass-dev.store" && <Route index={true} element={<HolyMassPage/>}/>}
+        {window.location.hostname === churchDomain && <Route index={true} element={<HolyMassPage/>}/>}
         {AppRoutes.map((route, index) => {
           const {element, ...rest} = route;
           return <Route key={index} {...rest} element={element}/>;
