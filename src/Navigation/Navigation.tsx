c94716a223d78856943fd65e979b6105a8a79e0e
Stanislau Zakharevich <stanislau.zakharevich@gmail.com>
2023-07-06 18:06:14 +0200

    fix(Navigation): redirect to another domain

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -10,8 +10,10 @@ const Navigation = () => {
     <div className="body-container">
       <Routes>
         {hostname === "www.holymass-dev.store" ? <Route index={true} element={<HolyMassPage/>}/> : null}
-        {hostname === "www.holymass-dev.store" && window.location.pathname !== '/' ? window.location.href = "www.catholic-dev.store" : null}
-        {window.location.pathname === '/holy-mass' ? window.location.href= 'www.holymass-dev.store' : null}
+        {hostname === "www.holymass-dev.store" && window.location.pathname !== '/'
+          ? window.location.href = `http://www.catholic-dev.store${window.location.pathname}`
+          : null}
+        {window.location.pathname === '/holy-mass' ? window.location.href= 'http://www.holymass-dev.store' : null}
 
         {AppRoutes.map((route, index) => {
           const {element, ...rest} = route;
