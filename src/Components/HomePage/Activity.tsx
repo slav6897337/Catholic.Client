c827063d828c7444112c803f40001424941a6bfa
Stanislau Zakharevich <stanislau.zakharevich@gmail.com>
2023-12-13 12:18:20 +0100

    fix(Activities): styling

--- src/Components/HomePage/Activity.tsx
+++ src/Components/HomePage/Activity.tsx
@@ -19,9 +19,11 @@ const Activity = (props: IActivity) => {
       <div className="activityImageContainer">
          <img className="activityImage" src={props.image} alt={props.title} width='388' height='270'/>
       </div>
-      <div>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
+      <div className="activity__text_container">
+        <div className="activity__title">
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </div>
         <div className="activity__link">
           <p>Read More</p>
           <img className="activity__link_arrow" style={{width:25, height:10}} src='/img/arrow.png' alt='arrow'/>
